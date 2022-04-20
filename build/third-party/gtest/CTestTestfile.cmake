7d85e52a59a9bb6abcf3ff7e9d5bcd96ed2481a2
DmitriiDo <FRXBlack@mail.ru>
2022-04-20 23:49:40 +0300

    fix

--- build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/a1231/Desktop/lab05/third-party/gtest
-# Build directory: /Users/a1231/Desktop/lab05/build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/third-party/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/a1231/Desktop/lab05/third-party/gtest/googletest
-# Build directory: /Users/a1231/Desktop/lab05/build/third-party/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
